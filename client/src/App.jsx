c91b613586e14a9706d7ae304854f7e0e3310055
doecode <doecode@192.168.1.2>
2022-09-02 18:14:10 +0200

    moved redirection to the App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState, useEffect } from 'react'
-import { Route } from "wouter";
+import { Route, useLocation } from "wouter";
 import reactLogo from './assets/react.svg'
 import './App.css'
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
 
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 
 function App() {
+  const [location, setLocation] = useLocation();
+  const [user, loading] = useAuthState(auth);
+
+  //if the user is not authenticated, redirect to the login page
+  useEffect(()=>{
+    if(user === null && !loading){
+        setLocation("/login");
+    }
+  }, [user, loading])
 
   return (
     <div className="App">
