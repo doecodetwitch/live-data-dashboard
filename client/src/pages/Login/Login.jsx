3ccc36e25eb904c4d4006f5f3a56d9700cfb8eff
doecode <doecode@192.168.1.2>
2022-09-02 19:44:02 +0200

    added error handling for authentication

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,9 +1,15 @@
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useSignInWithEmailAndPassword, useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase';
+import { useLocation } from "wouter";
+import './Login.css';
 
 function Login () {
+    const [location, setLocation] = useLocation();
     const [userState, loadingState, errorState] = useAuthState(auth);
+    const [shouldDisplayWrongPassMessage, setShouldDisplayWrongPassMessage] = useState(false);
+    const [shouldDisplayDefaultErrorMessage, setShouldDisplayDefaultErrorMessage] = useState(false);
 
     const [
         signInWithEmailAndPassword,
@@ -17,6 +23,35 @@ function Login () {
     const onSubmit = (data) => {
         signInWithEmailAndPassword(data.email, data.password)
     };
+
+    useEffect(()=>{
+        if(error?.message === 'Firebase: Error (auth/wrong-password).') {
+            setShouldDisplayWrongPassMessage(true);
+        } else if(error?.message){
+            setShouldDisplayDefaultErrorMessage(true);
+        }
+    }, [error])
+
+    useEffect(()=>{
+        if(userState){
+            setLocation("/");
+        }
+    }, [userState])
+
+
+    const wrongPasswordMessage = (
+        <div className="errorModal">
+            <p>This is a wrong password, please try again.</p>
+            <button onClick={()=>(setShouldDisplayWrongPassMessage(false))}>OK</button>
+        </div>
+    );
+
+    const defaultErrorMessage = (
+        <div className="errorModal">
+            <p>Authentication failed, please try again later.</p>
+            <button onClick={()=>(setShouldDisplayDefaultErrorMessage(false))}>OK</button>
+        </div>
+    );
 
     return (
         <>
@@ -37,6 +72,9 @@ function Login () {
                     <button type="submit">Submit</button>
                 </form>
             </div>
+
+            {shouldDisplayWrongPassMessage ? wrongPasswordMessage : null}
+            {shouldDisplayDefaultErrorMessage ? defaultErrorMessage : null}
         </>
     );
 }
